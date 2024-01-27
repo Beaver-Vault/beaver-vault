2a0aa78bd4e41b1759cc2ea1f95a45b15baccf98
NaederJ <naederj@oregonstate.edu>
2024-01-27 17:24:40 -0500

    Connected frontend to Auth0, got login working

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
