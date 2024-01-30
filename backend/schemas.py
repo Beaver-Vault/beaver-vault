3c5c26f6e7d555ae3e76e9401aa3a82e80885120
NaederJ <naederj@oregonstate.edu>
2024-01-29 20:57:08 -0500

    Sorting through backend stuff

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,14 +1,24 @@
 from typing import Union
 from pydantic import BaseModel
 
-class UserBase(BaseModel):
-    username: str
 
-class UserCreate(UserBase):
-    password: str
+class User(BaseModel):
+    userID: Union[str, None] = None
+    email: str
+    accountCreationDate: Union[str, None] = None
+    lastLoginDate: Union[str, None] = None
 
-class User(UserBase):
-    id: int
 
-    class Config:
-        orm_mode = True
+# class UserBase(BaseModel):
+#     username: str
+#
+#
+# class UserCreate(UserBase):
+#     password: str
+#
+#
+# class User(UserBase):
+#     id: int
+#
+#     class Config:
+#         orm_mode = True
