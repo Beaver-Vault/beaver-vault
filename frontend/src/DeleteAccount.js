943216d6625d1f863b6755e66209d2e9c86cd27d
KS <sevierk@oregonstate.edu>
2024-02-24 18:02:50 -0800

    button work

--- frontend/src/DeleteAccount.js
+++ frontend/src/DeleteAccount.js
@@ -1,1 +1,10 @@
-// create a function that will delete the account.
+import axios from 'axios';
+
+export async function deleteUser(userId) {
+  try {
+    const response = await axios.delete(`http://localhost:8000/users/${userId}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,8 +1,10 @@
-import { Box, Typography, ButtonBase, Button } from "@mui/material";
+import React from 'react';
+import { Box, Typography, ButtonBase, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "./slices/authSlice";
 import { clearInfo } from "./slices/userInfoSlice";
+import { deleteUser } from "./deleteAccount";
 import BeaverLogo from "./imgs/beaver_logo.png";
 
 export default function NavBar() {
@@ -11,10 +13,25 @@ export default function NavBar() {
 
   const loggedInUser = useSelector((state) => state.auth.user);
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleSignOut = () => {
     dispatch(logout());
     dispatch(clearInfo());
     navigate("/");
+  };
+
+  const handleDeleteAccount = () => {
+    dispatch(deleteUser(loggedInUser.id));
+    handleClose();
   };
 
   return (
@@ -46,6 +63,28 @@ export default function NavBar() {
             <Button variant="contained" onClick={handleSignOut}>
               Sign Out
             </Button>
+            <Button variant="contained" onClick={handleClickOpen}>
+              Delete Account
+            </Button>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">{"Delete Account"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Are you sure you want to delete your account? This action cannot be undone.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleDeleteAccount} autoFocus>
+                  Delete
+                </Button>
+              </DialogActions>
+            </Dialog>
           </Box>
         ) : (
           <> </>
